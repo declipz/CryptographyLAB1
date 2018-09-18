682399cef30cb386698ff7b9ca4ca46f0f43c5e2
Vladislav Kondrashkov <declipz@Vladislavs-Mac.local>
2018-09-18 11:44:32 +0300

    main view controller refactoring

--- cryptography/Encoder.swift
+++ cryptography/Encoder.swift
@@ -17,28 +17,6 @@ extension Character {
     }
 }
 
-extension Int {
-    // Euclidian equation for Greatest Common Divisor
-    static func gcd(_ firstNumber: Int, with secondNumber: Int) -> Int {
-        var buffer: Int = 0
-        var x = firstNumber
-        var y = secondNumber
-        while secondNumber != 0 {
-            buffer = y
-            y = x % y
-            x = buffer
-        }
-        return x
-    }
-    
-    static func isMutuallyPrimal(_ firstNumber: Int, and secondNumber: Int) -> Bool {
-        if Int.gcd(firstNumber, with: secondNumber) == 1 {
-            return true
-        }
-        return false
-    }
-}
-
 class Encoder {
     static let punctuationMarks = [",", ".", " ", "!", "?", ":", ";"]
     
--- cryptography/ViewController.swift
+++ cryptography/ViewController.swift
@@ -8,7 +8,27 @@
 
 import Cocoa
 
-
+extension Int {
+    // Euclidian equation for Greatest Common Divisor
+    static func gcd(_ firstNumber: Int, with secondNumber: Int) -> Int {
+        var buffer: Int = 0
+        var x = firstNumber
+        var y = secondNumber
+        while y != 0 {
+            buffer = y
+            y = x % y
+            x = buffer
+        }
+        return x
+    }
+    
+    static func isMutuallyPrimal(_ firstNumber: Int, and secondNumber: Int) -> Bool {
+        if Int.gcd(firstNumber, with: secondNumber) == 1 {
+            return true
+        }
+        return false
+    }
+}
 
 
 class ViewController: NSViewController {
@@ -31,148 +51,39 @@ class ViewController: NSViewController {
     
     @IBAction func encodeButtonAction(_ sender: NSButton) {
         let sourceText = encodingSourceTextField.stringValue
-        let firstKey = Int(encodingFirstKeyField.stringValue)!
-        let secondKey = Int(encodingSecondKeyField.stringValue)!
-        let module = Int(encodingModuleField.stringValue)!
-        let encodedText = encodeText(sourceText, firstKey, secondKey, module)
+        guard sourceText.isEmpty == false,
+            let firstKey = Int(encodingFirstKeyField.stringValue),
+            let secondKey = Int(encodingSecondKeyField.stringValue),
+            let module = Int(encodingModuleField.stringValue),
+            module == 26 || module == 33,
+            Int.isMutuallyPrimal(firstKey, and: module) == true,
+            Int.isMutuallyPrimal(secondKey, and: module) == true else {
+                return
+        }
         
+        let encodedText = Encoder.encodeText(sourceText, with: firstKey, and: secondKey, by: module)
         encodingEncodedTextField.stringValue = encodedText
-    
     }
     
     
     @IBAction func decodeButtonAction(_ sender: NSButton) {
         let encodedText = decodingEncodedTextField.stringValue
-        let firstKey = Int(decodingFirstKeyField.stringValue)!
-        let secondKey = Int(decodingSecondKeyField.stringValue)!
-        let module = Int(decodingModuleField.stringValue)!
-        let decodedText = decodeText(encodedText, firstKey, secondKey, module)
+        guard encodedText.isEmpty == false,
+            let firstKey = Int(decodingFirstKeyField.stringValue),
+            let secondKey = Int(decodingSecondKeyField.stringValue),
+            let module = Int(decodingModuleField.stringValue),
+            module == 26 || module == 33,
+            Int.isMutuallyPrimal(firstKey, and: module) == true,
+            Int.isMutuallyPrimal(secondKey, and: module) == true else {
+                return
+        }
         
+        let decodedText = Encoder.decodeText(encodedText, with: firstKey, and: secondKey, by: module)
         decodingSourceTextField.stringValue = decodedText
     }
-    
-    func isPrimal(_ key: Int,_ module: Int) -> Bool{
-        var counter = 0
-        for i in 0...module {
-            if i != 0 {
-                if key % i == 0 && module % i == 0 {
-                    counter += 1;
-                }
-            }
-        }
-        if counter == 1 {
-            return true
-        }
-        else {
-            return false
-        }
-    }
 
-    
-    func decodeText(_ encodedText: String,_ firstKey: Int,_ secondKey: Int,_ module: Int) -> String {
-        var decodedText: String = ""
-        for symbol in encodedText {
-            var wholePart = 0
-            if punctuationMarks.contains(String(symbol)) {
-                decodedText += String(symbol)
-            }
-            else {
-                var makingCalculations = true
-                while makingCalculations {
-                    switch module {
-                    case 33:
-                        let index = symbol.code - 1040
-                        
-                        let decodedIndex = module * wholePart + index - firstKey
-                        if decodedIndex >= 0 {
-                            if decodedIndex % secondKey == 0 {
-                                let decodedSymbolCode = Int(decodedIndex / secondKey) + 1040
-                                let decodedSymbol = Character(UnicodeScalar(decodedSymbolCode)!)
-                                decodedText += String(decodedSymbol)
-                                makingCalculations = false
-                                break
-                            }
-                            else {
-                                wholePart += 1
-                            }
-                        }
-                        else {
-                            wholePart += 1
-                        }
-                        
-                    case 26:
-                        let index = symbol.code - 65
-                        
-                        let decodedIndex = module * wholePart + index - firstKey
-                        if decodedIndex >= 0 {
-                            if decodedIndex % secondKey == 0 {
-                                let decodedSymbolCode = Int(decodedIndex / secondKey) + 65
-                                let decodedSymbol = Character(UnicodeScalar(decodedSymbolCode)!)
-                                decodedText += String(decodedSymbol)
-                                makingCalculations = false
-                                break
-                            }
-                            else {
-                                wholePart += 1
-                            }
-                        }
-                        else {
-                            wholePart += 1
-                        }
-                    default:
-                        
-                        // Make an error
-                        return ""
-                    }
-                }
-            }
-        }
-        return decodedText
-    }
-    
-    func encodeText(_ sourceText: String,_ firstKey: Int,_ secondKey: Int,_ module: Int) -> String {
-        var encodedText: String = ""
-        for symbol in sourceText {
-            if punctuationMarks.contains(String(symbol)) {
-                encodedText += String(symbol)
-            }
-            else {
-                switch module {
-                case 33:
-                    let index = symbol.code - 1040
-                    if index < 0 || index > 31 {
-                        // Make an error
-                    }
-                    let newSymbolCode = (index * secondKey + firstKey) % module + 1040
-                    let newSymbol = Character(UnicodeScalar(newSymbolCode)!)
-                    encodedText += String(newSymbol)
-                case 26:
-                    let index = symbol.code - 65
-                    if index < 0 || index > 25 {
-                        // Make an error
-                    }
-                    let newSymbolCode = (index * secondKey + firstKey) % module + 65
-                    let newSymbol = Character(UnicodeScalar(newSymbolCode)!)
-                    encodedText += String(newSymbol)
-                default:
-                    // Make an error
-                    return ""
-                }
-            }
-
-        }
-        return encodedText
-    }
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let encodedText = Encoder.encodeText("HELLO WORLD", with: 3, and: 5, by: 26)
-        print(Encoder.decodeText(encodedText, with: 3, and: 5, by: 26))
-        
-        
-        
-        
     }
 
     override var representedObject: Any? {
